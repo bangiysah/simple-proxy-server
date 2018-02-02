8cf398628b550ab46c5aac7f1ff7b7573728d940
HarshaVardhan <bangiysah@gmail.com>
2018-02-02 18:30:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# simple-proxy-server
-Multithreaded proxy server with caching
+#Simple Multi-threaded proxy server with caching
+
+This project was implemented in python.
+
+1. In this project a client has to insert URL and the script parses that URL according to HTTP protocol.
+2. It searches for the requested page in cache.If it is found it responses with the requested page.
+3. Otherwise it tries to fetch the requested page from the server and sends responses
+   to client and save one copy for its future referneces.
+4. For caching we made use of dictionary in python.
+
+#Testing:-
+
+1. you can test the proxy server function using telent protocol
+
+ EX:-
+    telnet cis.poly.edu 80
+
+	GET /~ross/ HTTP/1.1
+	Host: cis.poly.edu
+
+	This opens a TCP connection to port 80 of the host cis.poly.edu and then sends the 
+	HTTP request message.You can see the raw base HTML files in the command prompt.
+
+2. You can enter the url in the browser in the below format.
+
+   http:// < ip-address >:< port_no >/ < URL >
+
+ EX:-
+    http://localhost:8888/www.google.com
+ 
+ Note :- 
+   1. This works only for HTTP protocol(not for HTTPS).
+   2. The topic of proxy server was much elaborated in Kurose-Ross Computer-Networking textbook.
+   3. If any errors found email - harshavardhanbangi.2014@gmail.com
---
+++ proxy_server.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+import sys
+import socket
+import itertools
+import signal
+import argparse
+from time import sleep
+from socket import socket as Socket
+from threading import *
+from HTTP_request_handler import *
+
+BUFFER_SIZE = 4096
+REMOTE_PORT = 80
+
+def main():
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--port', '-p', default=8888, type=int, help='Port to use')
+	args = parser.parse_args()
+	global Threads
+
+	try:
+		server_socket = Socket(socket.AF_INET, socket.SOCK_STREAM)
+		server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+		server_socket.bind(('', args.port))
+		server_socket.listen(100)
+
+		cache_dict = {}
+		print "Proxy server is running.....\n"
+
+		while True:			
+			try:
+				connection_socket , connection_addr = server_socket.accept()
+
+				t = Thread(target = http_request_parse, args = (cache_dict , connection_socket ,))
+				print "Connected client :" , connection_addr
+				t.setDaemon(1)
+				t.start()
+				t.join()
+				signal.signal(signal.SIGINT,exit_handler)
+				signal.pause()
+			except socket.error as e:
+				print e
+			finally:
+				connection_socket.close()
+
+	except KeyboardInterrupt as msg:
+		print msg
+		sys.exit()
+	finally:
+		server_socket.close()
+    
+
+def http_request_parse(cache_dict , connection_socket):
+
+	request_string = connection_socket.recv(BUFFER_SIZE).decode('utf-8','ignore')
+	request = HTTPRequest(request_string)
+
+	method = request.command
+	uri = request.path
+
+	if method == "GET":
+		pass
+	else:
+		print "It is not GET method"
+		sys.exit()
+	    
+	try:
+		useless, full_url, path = uri.split('/',2)
+	except ValueError:
+		useless = None
+		l,full_url = uri.split('/',1)
+		path = ''
+
+	search_key = request.path
+
+	if search_key in cache_dict:
+		cached_page = cache_dict.get(search_key)
+		print "Got page from", search_key ,"cache\n\n"
+		
+	else:
+		cached_page = http_response(full_url , path)
+		print(cached_page)
+		cache_dict[search_key] = cached_page
+		print "Serving page from", search_key ,"and cached it\n\n"
+		
+	connection_socket.send(cached_page.encode('utf-8'))
+
+
+
+def http_response(full_url , path):
+
+	global reply
+
+	try:
+		remote_ip = socket.gethostbyname( full_url )
+		print(remote_ip)		
+	except socket.gaierror:
+		print "Hostname could not be resolved. Exiting"
+		sys.exit()
+
+	client_socket = Socket(socket.AF_INET, socket.SOCK_STREAM)
+	client_socket.connect((remote_ip, REMOTE_PORT))
+
+	if path is None:
+		message = "GET / " +"HTTP/1.0\r\nHost: " +  full_url+"\r\n\r\n"
+	else:
+		message = "GET /" + path +" HTTP/1.0\r\nHost: " + full_url+"\r\n\r\n"
+
+	try :
+		client_socket.sendall((message).encode('utf-8'))
+	except socket.gaierror:
+		print "Send failed"
+		sys.exit()
+
+	reply = client_socket.recv(4096).decode('utf-8','ignore')
+	client_socket.close()
+
+	return reply
+
+def exit_handler(signal , frame):
+	print "Main thread exitted successfully"
+	sys.exit()
+
+
+if __name__ == '__main__':
+	main()
+	sys.exit()   
